689976309aa5faa9917857032d89d6304587b05e
橋本洋平 <y.hashimoto.enmore.engineer@gmail.com>
2026-04-09 19:06:03 +0900

    feat: shadcn-svelteのサイドバー実装をコピペして土台作成

---
+++ src/lib/components/app-sidebar.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+ import CalendarIcon from "@lucide/svelte/icons/calendar";
+ import LayoutDashboard from "@lucide/svelte/icons/layout-dashboard"
+ import InboxIcon from "@lucide/svelte/icons/inbox";
+ import SearchIcon from "@lucide/svelte/icons/search";
+ import SettingsIcon from "@lucide/svelte/icons/settings";
+ import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+ 
+ // Menu items.
+ const items = [
+  {
+   title: "ダッシュボード",
+   url: "#",
+   icon: LayoutDashboard,
+  },
+  {
+   title: "Inbox",
+   url: "#",
+   icon: InboxIcon,
+  },
+  {
+   title: "Calendar",
+   url: "#",
+   icon: CalendarIcon,
+  },
+  {
+   title: "Search",
+   url: "#",
+   icon: SearchIcon,
+  },
+  {
+   title: "Settings",
+   url: "#",
+   icon: SettingsIcon,
+  },
+ ];
+</script>
+ 
+<Sidebar.Root>
+ <Sidebar.Content>
+  <Sidebar.Group>
+   <!-- <Sidebar.GroupLabel>choi-put</Sidebar.GroupLabel> -->
+   <Sidebar.GroupContent>
+    <Sidebar.Menu>
+     {#each items as item (item.title)}
+      <Sidebar.MenuItem>
+       <Sidebar.MenuButton>
+        {#snippet child({ props })}
+         <a href={item.url} {...props}>
+          <item.icon />
+          <span>{item.title}</span>
+         </a>
+        {/snippet}
+       </Sidebar.MenuButton>
+      </Sidebar.MenuItem>
+     {/each}
+    </Sidebar.Menu>
+   </Sidebar.GroupContent>
+  </Sidebar.Group>
+ </Sidebar.Content>
+</Sidebar.Root>
